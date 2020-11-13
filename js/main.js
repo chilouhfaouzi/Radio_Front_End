ef315a097ec0cb554dd37da3f02fc3212c031e4b
fawzichilouh <61601372+fawzichilouh@users.noreply.github.com>
2020-11-13 23:09:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,3 +12,32 @@ $(".side-list li").click(function () {
   $(".side-list li").removeClass("active");
   $(this).addClass("active");
 });
+
+// play les stations des radios
+
+$(".play-radio").click(function () {
+  var all = $(".myAudio");
+
+  for (var i = 0; i < all.length; i++) {
+    all[i].pause();
+  }
+
+  $(this).parent().find("#myAudio")[0].play();
+  var pauses = $(".pause");
+  var playes = $(".play-radio").each(function () {
+    $(this).show();
+  });
+  pauses.each(function () {
+    $(this).hide();
+  });
+  $(this).hide();
+
+  $(this).parent().find(".pause").css("display", "inline");
+});
+
+// Pauses les stations des radios
+$(".pause").click(function () {
+  $(this).hide();
+  $(this).parent().find("#myAudio")[0].pause();
+  $(this).parent().find(".play-radio").css("display", "inline");
+});
